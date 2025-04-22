dda2204a54965ac5035fa5785e3baba7142a231b
ROSI STORE <fathorrosi606@gmail.com>
2025-04-22 11:34:21 +0700

    Delete SKT-sslh-fix directory

--- SKT-sslh-fix/sslh-fix-bug.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# ==========================================
-
-
-echo "10 4 * * * root clearlog && sslh-fix-reboot" >> /etc/crontab
-echo "10 5 * * * root clearlog && sslh-fix-reboot" >> /etc/crontab
